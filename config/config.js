cdb3ae36cf71565361033cc01e4cf9ac0bda30ab
Florian <florianbematol@gmail.com>
2020-06-30 19:01:43 +0200

    Inital commit

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    development: {
+        username: 'postgres',
+        password: '123',
+        database: 'project_vacance_entre_amis_development',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+        logging: (msg) => console.log('[DATABASE]', msg)
+    },
+    test: {
+        username: 'postgres',
+        password: '123',
+        database: 'project_vacance_entre_amis_test',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+        logging: false
+    },
+    production: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        logging: (msg) => console.log('[DATABASE]', msg)
+    }
+}
