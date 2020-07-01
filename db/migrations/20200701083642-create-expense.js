c59fb93e515710278292195866961c36d89a729f
Florian <florianbematol@gmail.com>
2020-07-01 11:56:39 +0200

    Update migrations + seeds order

--- db/migrations/20200701083642-create-expense.js
+++ db/migrations/20200701083642-create-expense.js
@@ -8,6 +8,16 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      eventId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Events',
+            key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
       name: {
         type: Sequelize.STRING
       },
