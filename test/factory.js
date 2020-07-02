77d6fb22f97e3a878f78a81cb648eb7c78ca1120
unknownpanda <41007424+florianbematol@users.noreply.github.com>
2020-07-02 15:54:01 +0200

    Create event #6 (#15)
    * Modification middlewares parsing form
    
    * Ajout framework css Materialize + custom css
    
    * Creation event - router + tests
    
    * Ecriture des test pour la création d'un Event couche Service
    
    * Creation Event - repostiory - code + test
    
    * Creation Event - service - gestion si la dateEnd plus petit que dateStart
    
    * Creation Event - service - Fix erreur règles de validation
    
    * Creation Event - view - Fix erreur missing variable

--- test/factory.js
+++ test/factory.js
@@ -1,6 +1,25 @@
 const models = require('../lib/models')
+const Event = models.Event
+
 
 const factory = {
+    createEventData: ({
+        name = 'Petite vacance chill',
+        dateStart = new Date(2020, 10, 24),
+        dateEnd = new Date(2020, 11, 5),
+        place = 'Dubai'
+    } = {}) => {
+        return { name, dateStart, dateEnd, place }
+    },
+    createEvent: ({
+        id = null,
+        name = 'Petits vacance chill',
+        dateStart = new Date(2020, 10, 24),
+        dateEnd = new Date(2020, 11, 5),
+        place = 'Dubai'
+    } = {}) => {
+        return new Event({ id, name, dateStart, dateEnd, place })
+    }
 }
 
 module.exports = factory
